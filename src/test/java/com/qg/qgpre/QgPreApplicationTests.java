f7624a52a86bf145c32ab525a09479dd8f817408
29081 <xiepanpan1995@163.com>
2019-10-03 22:28:33 +0800

    Initial commit

---
+++ src/test/java/com/qg/qgpre/QgPreApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qg.qgpre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QgPreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
